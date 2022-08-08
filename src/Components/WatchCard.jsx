da2058a3abc7d724f7a89321f9ea34f0c66e87dd
Anowar Hossain <anowar3042@gmail.com>
2022-08-08 17:31:30 +0530

    done

--- src/Components/WatchCard.jsx
+++ src/Components/WatchCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { useSelector } from "react-redux";
 
 const WatchCard = ({ id, name, image, category }) => {
   return (
